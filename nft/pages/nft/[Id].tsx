2c1b588986bc61f8fc98a85c19c27c92e694d68c
Dav420I <david.arutyunyan.97@bk.ru>
2022-08-01 01:07:12 +0300

    commit path1 apes

--- nft/pages/nft/[Id].tsx
+++ nft/pages/nft/[Id].tsx
@@ -1,7 +1,40 @@
 import React from 'react'
 
 function NFTDropPage () {
-    return <div> This is the NFTDropPage</div>
+    return <div className='flex h-screen flex-col'>
+    {/*left*/}
+    <div className='bg-gradient-to-br from-cyan-400
+    to-rose-300'>
+        <div className='flex flex-col items-center justify-center py-2
+        lg:min-h-screen'>
+            <div className='rounded-xl bg-gradient-to-br
+            from-yellow-400 to-purple-600 p-2'>
+                <img className='w-44 rounded-xl
+            object-cover lg:h-96 lg:w-72'
+                     src="https://links.papareact.com/8sg" alt=""/>
+            </div>
+
+            <h1 className='text-4xl font bold
+            text-white'>
+                DAVI420I Apes
+            </h1>
+            <h2 className='text-xl
+            text-grey-300'>
+                A collection of DAVI420I Apes who live & breathe React!
+            </h2>
+        </div>
+    </div>
+
+    {/*right*/}
+
+        <div>
+
+        </div>
+
+    </div>
+
+
+
 }
 
 export default NFTDropPage
