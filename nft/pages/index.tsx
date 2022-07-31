f07d2a181c5a6fc472432718ffe7eb9931e87714
Dav420I <david.arutyunyan.97@bk.ru>
2022-08-01 00:09:21 +0300

    Initial commit

---
+++ nft/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage =  () => {
+    return (
+        <div className="">
+            <Head>
+                <title>NFT Drop</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <h1 className=' '>Welcone to the NFT DROP</h1>
+        </div>
+    )
+}
+
+export default Home
---
+++ nft/pages/nft/[Id].tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function NFTDropPage () {
+    return <div> This is the NFTDropPage</div>
+}
+
+export default NFTDropPage
+
